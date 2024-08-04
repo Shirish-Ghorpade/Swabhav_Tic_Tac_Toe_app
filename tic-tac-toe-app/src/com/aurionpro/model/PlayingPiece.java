2694f8bb45e7349c859fab567cc98de8925fb784
Shirish-Ghorpade <97929917+Shirish-Ghorpade@users.noreply.github.com>
2024-08-04 12:48:34 +0530

    Delete tic-tac-toe-app directory

--- tic-tac-toe-app/src/com/aurionpro/model/PlayingPiece.java
+++
@@ -1,9 +0,0 @@
-package com.aurionpro.model;
-
-public class PlayingPiece {
-	public PieceType pieceType;
-
-	public PlayingPiece(PieceType pieceType) {
-		this.pieceType = pieceType;
-	}
-}
--- tic-tac-toe-app/src/com/aurionpro/model/PlayingPieceX.java
+++
@@ -1,9 +0,0 @@
-package com.aurionpro.model;
-
-public class PlayingPieceX extends PlayingPiece{
-
-	public PlayingPieceX() {
-		super(PieceType.X);
-		// TODO Auto-generated constructor stub
-	}
-}
